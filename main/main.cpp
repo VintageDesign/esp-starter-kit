56b712f04d9e43e6ddc61b4958e7a122eb946696
Riley Mathieu <riley.mathieu@ravenind.com>
2025-08-13 08:06:40 -0400

    Test steppers

--- main/main.cpp
+++ main/main.cpp
@@ -2,19 +2,20 @@
 #include <freertos/task.h>
 #include <inttypes.h>
 
+#include <math.h>
 #include <servo.h>
-
+#include <stepper.h>
 
 static const char *TAG = "APP";
-extern "C" void app_main()
-{
-    Servo servo;
-    servo.attach(23);
-    while(true)
-    {
-        servo.write(0);
-        vTaskDelay(1000/portTICK_PERIOD_MS);
-        servo.write(100);
-        vTaskDelay(1000/portTICK_PERIOD_MS);
-    }
+extern "C" void app_main() {
+  Stepper stepper(22, 5, 23, MICROSTEP_1, 240);
+  stepper.enable_motor();
+  while (true) {
+    stepper.set_dir(false);
+    stepper.write_rad(M_PI);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    stepper.set_dir(true);
+    stepper.write_rad(M_PI);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }
 }
